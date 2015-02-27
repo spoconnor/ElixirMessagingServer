347d9a1c4bb8ed293499f2179ed733d8cb846050
Sean <onewheel@gmail.com>
2015-02-27 21:04:13 +1300

    tag riak_pb build

--- ElixirMessagingServer/mix.exs
+++ ElixirMessagingServer/mix.exs
@@ -28,6 +28,7 @@ defmodule ElixirMessagingServer.Mixfile do
       {:lager, github: "basho/lager" },
       {:jiffy, github: "davisp/jiffy" },
       {:ibrowse, github: "cmullaparthi/ibrowse" },
+      {:riak_pb, github: "basho/riak_pb", override: true, tag: "2.0.0.16", compile: "./rebar get-deps compile deps_dir=../"},
       {:riakc, github: "basho/riak-erlang-client"},
       {:eper, github: "massemanet/eper" },
       {:mixer, github: "opscode/mixer" },
