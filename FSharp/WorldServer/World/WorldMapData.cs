a915d9679c7ed9931510814c315fc6f33f731baa
Sean <onewheel@gmail.com>
2015-04-16 19:48:19 +1200

    ...

--- FSharp/WorldServer/World/WorldMapData.cs
+++ FSharp/WorldServer/World/WorldMapData.cs
@@ -2,51 +2,49 @@
 
 namespace Sean.World
 {
+    internal class MapCell
+    {
+        public MapCell()
+        {
+        }
 
-	internal class MapCell
-	{
-		public MapCell()
-		{
-		}
+    }
 
+    internal class WorldMapData
+    {
+        public WorldMapData()
+        {
+            Octaves = 3;
+            MinY = 0;
+            MaxY = 100;
+            SizeX = 20;
+            SizeZ = 40;
+        }
 
-	}
-
-	internal class WorldMapData
-	{
-		public WorldMapData()
-		{
-			Octaves = 3;
-			MinY = 0;
-			MaxY = 100;
-			SizeX = 20;
-			SizeZ = 40;
-		}
-
-		public void Generate()
-		{
+        public void Generate()
+        {
             heightMap = PerlinNoise.GetIntMap(SizeX+1, SizeZ+1, MinY, MaxY, Octaves);
-            mapCells = Misc.GetEmptyArray<int>(SizeX, SizeZ);
-			for (int x = 0; x < SizeX; x++)
-			{
-				System.Text.StringBuilder builder = new System.Text.StringBuilder();
-				for (int z = 0; z < SizeZ; z++)
-				{
+            mapCells = Misc.GetEmptyArray<MapCell>(SizeX, SizeZ);
+            for (int x = 0; x < SizeX; x++)
+            {
+                System.Text.StringBuilder builder = new System.Text.StringBuilder();
+                for (int z = 0; z < SizeZ; z++)
+                {
                     builder.Append(heightMap[x][z] / 10);
-				}
-				Console.WriteLine(builder.ToString());
-			}
-		}
+                }
+                Console.WriteLine(builder.ToString());
+            }
+        }
 
-		public int Octaves { get; set; }
-		public int MinY { get; set; }
-		public int MaxY { get; set; }
-		public int SizeX { get; set; }
-		public int SizeZ { get; set; }
+        public int Octaves { get; set; }
+        public int MinY { get; set; }
+        public int MaxY { get; set; }
+        public int SizeX { get; set; }
+        public int SizeZ { get; set; }
 
         private int[][] heightMap;
         private MapCell[][] mapCells;
-	}
+    }
 
 }
 
