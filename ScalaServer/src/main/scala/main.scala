0c12ad5e2e183e213403898c5d341af1416c9355
Sean <onewheel@gmail.com>
2015-08-27 21:49:40 +1200

    nearly

--- ScalaServer/src/main/scala/main.scala
+++ ScalaServer/src/main/scala/main.scala
@@ -1,19 +1,30 @@
 import akka.actor._
+import akka.io.{ IO, Tcp }
+import java.net.InetSocketAddress
 
-/**
- * @author sean
- */
-object main extends App {
-  Console.println("Hello World")
-  var perlin = new perlinNoise.PerlinNoise()
-  //var noise = perlin.GenerateWhiteNoise(10,10)
-  var noise = perlin.GetIntMap(80, 30, 0, 9, 3)
-  noise.Dump()
-  
-  val system = ActorSystem("NetworkServiceSystem")
-  val endpoint = new InetSocketAddress("localhost", 9999)
-  val server = system.actorOf(NetworkService.props(endpoint), "network-service")
+object App extends App {
+  final val VERSION = "1.0-SCALASERVICE"
+  final val CODENAME = "Scala Service"
 
-  readLine("Hit ENTER to exit...")
-  system.shutdown()
+  implicit var actorSystem: ActorSystem = _
+
+  override def main() 
+  {
+    Console.println("Hello World")
+    var perlin = new perlinNoise.PerlinNoise()
+    //var noise = perlin.GenerateWhiteNoise(10,10)
+    var noise = perlin.GetIntMap(80, 30, 0, 9, 3)
+    noise.Dump()
+
+    Console.println("Loading new Actor System")
+    val actorSystem = ActorSystem("NetworkServiceSystem")
+
+    Console.println("Starting Network Service")
+    val endpoint = new InetSocketAddress("localhost", 9999)
+    val handler = actorSystem.actorOf(Props[NetworkService], name = "network-handler")
+    IO(Tcp) ! Tcp.Bind(handler, endpoint)
+
+    readLine("Hit ENTER to exit...")
+    actorSystem.shutdown()
+  }
 }
--- ScalaServer/src/main/scala/networkservice.scala
+++ ScalaServer/src/main/scala/networkservice.scala
@@ -8,6 +8,38 @@ object NetworkService {
     Props(new NetworkService(endpoint))
 }
 
-class NetworkService(endpoint: InetSocketAddress) extends Actor with ActorLogging {
-  IO(Tcp) ! Tcp.Bind(self, endpoint)
+class NetworkService(endpoint: InetSocketAddress) extends Actor with akka.actor.ActorLogging {
+  override def receive: Receive = {
+    case Tcp.Connected(remote, _) =>
+      log.debug("Remote address {} connected", remote)
+      sender ! Tcp.Register(context.actorOf(EchoConnectionHandler.props(remote, sender)))
+  }
+
+}
+
+object EchoConnectionHandler {
+  def props(remote: InetSocketAddress, connection: ActorRef): Props =
+    Props(new EchoConnectionHandler(remote, connection))
+}
+
+class EchoConnectionHandler(remote: InetSocketAddress, connection: ActorRef) extends Actor with ActorLogging {
+
+  // We need to know when the connection dies without sending a `Tcp.ConnectionClosed`
+  context.watch(connection)
+
+  def receive: Receive = {
+    case Tcp.Received(data) =>
+      val text = data.utf8String.trim
+      log.debug("Received '{}' from remote address {}", text, remote)
+      text match {
+        case "close" => context.stop(self)
+        case _       => sender ! Tcp.Write(data)
+      }
+    case _: Tcp.ConnectionClosed =>
+      log.debug("Stopping, because connection for remote address {} closed", remote)
+      context.stop(self)
+    case Terminated(`connection`) =>
+      log.debug("Stopping, because connection for remote address {} died", remote)
+      context.stop(self)
+  }
 }
