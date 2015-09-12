36a8c561154772990b7f71a04ea0fba872c9089e
Sean <onewheel@gmail.com>
2015-09-13 10:21:32 +1200

    compiling

--- ScalaServer/src/main/scala/Map.scala
+++ ScalaServer/src/main/scala/Map.scala
@@ -3,16 +3,18 @@ import akka.actor.Props
 import akka.event.Logging
 
 class Map extends Actor {
+  import context.system
   var log = Logging(context.system, this)
-  def recevie = {
-    case "init" init()
-    case "get" chunk ! GetChunk(1,1)
-    case _ log.info("Map received unknown message")
+  //val chunk = system.actorOf(Props[MapChunk], name = "chunk1")
+  val chunk = system.actorOf(Props(new MapChunk(1,1)), name = "chunk-1-1")
+
+  def receive = {
+    case "init" => init
+    case "get" => chunk ! new GetChunk(1,1)
+    case _ => log.info("Map received unknown message")
   }
 
   def init = {
     log.info("init")
-    //val chunk = system.actorOf(Props[MapChunk], name = "chunk1")
-    val chunk = system.actorOf(Props(new MapChunk(1,1)), name = "chunk-1-1")
   }
 }
--- ScalaServer/src/main/scala/MapChunk.scala
+++ ScalaServer/src/main/scala/MapChunk.scala
@@ -2,17 +2,18 @@ import akka.actor.Actor
 import akka.actor.Props
 import akka.event.Logging
 
-class GetChunk(xc: Int, yc: Int)
+case class GetChunk(xc: Int, yc: Int)
 
 class MapChunk(xc: Int, yc: Int) extends Actor {
+  import context.system
   val x: Int = xc
   val y: Int = yc
   var log = Logging(context.system, this)
 
   def receive = {
-    case "init" log.info("init")
-    case GetChunk(x,y) getChunk(x,y)
-    case _ log.info("MapChunk received unknown message")
+    case "init" => log.info("init")
+    case GetChunk(x,y) => getChunk(x,y)
+    case _ => log.info("MapChunk received unknown message")
   }
   
   override def preStart() = {
