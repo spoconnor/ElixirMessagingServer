7898c3366bcb2468c2813118afdd27d1eb60947f
Sean <onewheel@gmail.com>
2015-10-21 08:03:10 +1300

    reconnect

--- ScalaServer/src/main/scala/TcpClient.scala
+++ ScalaServer/src/main/scala/TcpClient.scala
@@ -3,6 +3,7 @@ package com.example.akkaTcpChat
 
 import akka.actor._
 import akka.io._
+import akka.io.Tcp._
 import java.net.InetSocketAddress
 import akka.util.ByteString
 import scala.concurrent.duration._
@@ -11,6 +12,7 @@ class TcpClient(remote: InetSocketAddress) extends Actor with ActorLogging {
  
   import Tcp._
   import context.system
+  import scala.concurrent.ExecutionContext.Implicits.global
 
   log.info("TcpClient: initializing connection to {}", remote)
   IO(Tcp) ! Connect(remote)
@@ -21,11 +23,12 @@ class TcpClient(remote: InetSocketAddress) extends Actor with ActorLogging {
 
     case CommandFailed(_: Connect) =>
       log.error("TcpClient: connect failed")
-//TODO
-      //system.scheduler().scheduleOnce(5 seconds, self, "reconnect")
+      val s = sender
+      context.system.scheduler.scheduleOnce(5 seconds, self, "reconnect")
       //context stop self
  
     case c @ Connected(remote, local) =>
+      log.info("TcpClient: Connected")
       val connection = sender()
       connection ! Register(self)
       context become {
@@ -39,10 +42,11 @@ class TcpClient(remote: InetSocketAddress) extends Actor with ActorLogging {
         case "close" =>
           connection ! Close
         case _: ConnectionClosed =>
-          log.info("TcpClient: connection closed, reopening")
-//TODO
-          //system.scheduler().scheduleOnce(5 seconds, self, "reconnect")
-          //context stop self
+          log.info("TcpClient: connection closed")
+          //val s = sender
+          //context.system.scheduler.scheduleOnce(5 seconds, self, "reconnect")
+          // TODO - send message to parent context?
+          context stop self
       }
   }
 }
