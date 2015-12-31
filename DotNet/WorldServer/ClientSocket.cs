0d09065bf3a63bcd2524182b2a8e94f3319973e4
Sean <onewheel@gmail.com>
2015-12-31 15:07:03 +1300

    threading

--- DotNet/WorldServer/ClientSocket.cs
+++ DotNet/WorldServer/ClientSocket.cs
@@ -24,8 +24,8 @@ namespace Sean.World
         }
     }
 
-    public class SynchronousSocketClient {
-
+    public class SynchronousSocketClient : IDisposable
+    {
         public SynchronousSocketClient(string server, int serverPort)
         {
             remoteHost = Dns.GetHostEntry(server);
@@ -33,6 +33,29 @@ namespace Sean.World
             port = serverPort;
         }
 
+        // Dispose pattern
+        bool disposed = false;
+        public void Dispose()
+        { 
+            Dispose(true);
+            GC.SuppressFinalize(this);           
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return; 
+            if (disposing) {
+                // Free any other managed objects here.
+                if (socket != null) {
+                    socket.Shutdown (SocketShutdown.Both);
+                    socket.Close ();
+                }
+            }
+            // Free any unmanaged objects here.
+            disposed = true;
+        }
+
+        private Socket socket;
         private int port;
         private static IPHostEntry remoteHost;
         private IPAddress ipAddress;
@@ -45,25 +68,13 @@ namespace Sean.World
                 byte[] bytes = new byte[1024];
 
                 IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
-                Socket sender = new Socket(
+                socket = new Socket(
                     AddressFamily.InterNetwork, 
                     SocketType.Stream, ProtocolType.Tcp );
 
-                sender.Connect(remoteEP);
-                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
-
-                // Encode the data string into a byte array.
-                byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
-                int bytesSent = sender.Send(msg);
-
-                // Receive the response from the remote device.
-                int bytesRec = sender.Receive(bytes);
-                Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes,0,bytesRec));
-
-                // Release the socket.
-                sender.Shutdown(SocketShutdown.Both);
-                sender.Close();
-            } 
+                socket.Connect(remoteEP);
+                Console.WriteLine("Socket connected to {0}", socket.RemoteEndPoint.ToString());
+            }
             catch (ArgumentNullException ane) 
             {
                 Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
@@ -77,5 +88,34 @@ namespace Sean.World
                 Console.WriteLine("Unexpected exception : {0}", ex.ToString());
             }
         }
+
+        public void SendMessage()
+        {
+            try {
+                // Encode the data string into a byte array.
+                byte[] msg = Encoding.ASCII.GetBytes ("This is a test<EOF>");
+                int bytesSent = socket.Send (msg);
+            } catch (ArgumentNullException ane) {
+                Console.WriteLine ("ArgumentNullException : {0}", ane.ToString ());
+            } catch (SocketException se) {
+                Console.WriteLine ("SocketException : {0}", se.ToString ());
+            } catch (Exception ex) {
+                Console.WriteLine ("Unexpected exception : {0}", ex.ToString ());
+            }
+        }
+
+        public void RecvMessage()
+        {
+            try {
+                // Receive the response from the remote device.
+                int bytesRec = socket.Receive (bytes);
+                Console.WriteLine ("Echoed test = {0}", Encoding.ASCII.GetString (bytes, 0, bytesRec));
+            } catch (ArgumentNullException ane) {
+                Console.WriteLine ("ArgumentNullException : {0}", ane.ToString ());
+            } catch (SocketException se) {
+                Console.WriteLine ("SocketException : {0}", se.ToString ());
+            } catch (Exception ex) {
+                Console.WriteLine ("Unexpected exception : {0}", ex.ToString ());
+            }
+        }
     }
-}
